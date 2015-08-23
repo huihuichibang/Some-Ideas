72fa4578617590b2a7ba12f98174728dee03407c
李远路 <huihuichibang@gmail.com>
2015-08-23 20:15:33 +0800

    The first commit

---
+++ OneHours/OneHours/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OneHours
+//
+//  Created by 李远路 on 15/8/23.
+//  Copyright (c) 2015年 LightRain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
